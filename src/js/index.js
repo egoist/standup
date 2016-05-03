c0106bbfbbdc17e8e72c5722d844e2f4e9c5e6a7
EGOIST <0x142857@gmail.com>
2016-05-03 19:59:40 +0800

    pause and resume timer

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,12 @@ new Vue({
   data() {
     const defaultUserData = {
       workTime: 40,
-      restTime: 10
+      restTime: 10,
     }
     return {
       user: JSON.parse(localStorage.getItem('stand:user')) || defaultUserData,
-      remainTime: null
+      remainTime: null,
+      paused: false
     }
   },
   ready() {
@@ -33,11 +34,17 @@ new Vue({
       const data = JSON.stringify(this.user)
       localStorage.setItem('stand:user', data)
     },
+    handlePause() {
+      this.paused = !this.paused
+    },
     startTimer() {
       console.log('start')
       clearInterval(this.timer)
       this.remainTime = this.user.workTime * 60 * 1000
       this.timer = setInterval(() => {
+        if (this.paused) {
+          return
+        }
         this.remainTime -= 1000
         if (this.remainTime <= 0) {
           clearInterval(this.timer)
